120ab8e258260555725866bb8c16310b5089b33d
Fan Jin <fanjin1989@gmail.com>
2016-02-01 00:15:37 -0800

    update

--- frontend/src/app/config/index.js
+++ frontend/src/app/config/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 function getHost() {
+  console.log('1234');
   switch(process.env.NODE_ENV) {
     case 'production':
       return 'http://159.203.31.187';
@@ -9,7 +10,7 @@ function getHost() {
   }
 }
 
-var config = {
+const config = {
   SERVER_HOST: getHost(),
 };
 
