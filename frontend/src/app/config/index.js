ecd9de1ffda74a8a4b62a35f20c1dcd076f54fcf
Fan Jin <fanjin1989@gmail.com>
2016-01-31 23:56:38 -0800

    update

--- frontend/src/app/config/index.js
+++ frontend/src/app/config/index.js
@@ -1,3 +1,17 @@
-module.exports = {
-  SERVER_HOST: 'http://localhost:3000',
+"use strict";
+
+function getHost() {
+  switch(process.env.NODE_ENV) {
+    case 'production':
+      return 'http://localhost:3000';
+    default:
+      return 'http://localhost';
+  }
+}
+
+var config = {
+  SERVER_HOST: getHost(),
 };
+
+
+module.exports = config;
